8a47b04604e5b85e6c7b4720c4a673c769452ecb
kiber-io <60169133+kiber-io@users.noreply.github.com>
2024-10-09 16:17:50 +0400

    Make hooks more universal; Support Android 14

--- app/src/main/java/tw/idv/palatis/xappdebug/xposed/HookMain.java
+++ app/src/main/java/tw/idv/palatis/xappdebug/xposed/HookMain.java
@@ -2,8 +2,6 @@ package tw.idv.palatis.xappdebug.xposed;
 
 import static android.content.pm.ApplicationInfo.FLAG_DEBUGGABLE;
 import static android.util.Log.getStackTraceString;
-import static de.robv.android.xposed.XposedBridge.hookAllMethods;
-import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static tw.idv.palatis.xappdebug.Constants.CONFIG_PATH_FORMAT;
 import static tw.idv.palatis.xappdebug.Constants.LOG_TAG;
 
@@ -17,6 +15,8 @@ import android.os.UserHandle;
 
 import androidx.annotation.Keep;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -24,6 +24,7 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.SELinuxHelper;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 @Keep
@@ -32,186 +33,6 @@ public class HookMain implements IXposedHookLoadPackage {
     // taken from Zygote.java
     // https://android.googlesource.com/platform/frameworks/base.git/+/master/core/java/com/android/internal/os/Zygote.java
     private static final int DEBUG_ENABLE_JDWP = 1;
-
-    @Override
-    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (!"android".equals(lpparam.packageName))
-            return;
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            _hookPostTiramisu(lpparam);
-        } else {
-            _hookPreTiramisu(lpparam);
-        }
-    }
-
-    private void _hookPostTiramisu(final XC_LoadPackage.LoadPackageParam lpparam) {
-        final String PM_CLASS = "com.android.server.pm.ComputerEngine";
-
-        findAndHookMethod(
-                PM_CLASS,
-                lpparam.classLoader,
-                "getPackageInfo",
-                String.class, long.class, int.class, /* packageName, flags, userId */
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        try {
-                            PackageInfo packageInfo = (PackageInfo) param.getResult();
-                            if (packageInfo != null && packageInfo.applicationInfo != null)
-                                checkAndMakeDebuggable(packageInfo.applicationInfo, packageInfo.packageName, (int) param.args[2]);
-                        } catch (Exception e) {
-                            XposedBridge.log(LOG_TAG + ": " + getStackTraceString(e));
-                        }
-                    }
-                }
-        );
-
-        findAndHookMethod(
-                PM_CLASS,
-                lpparam.classLoader,
-                "getApplicationInfo",
-                String.class, long.class, int.class, /* packageName, flags, userId */
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        try {
-                            ApplicationInfo appInfo = (ApplicationInfo) param.getResult();
-                            if (appInfo != null)
-                                checkAndMakeDebuggable(appInfo, ((ApplicationInfo) param.getResult()).packageName, (int) param.args[2]);
-                        } catch (Exception e) {
-                            XposedBridge.log(LOG_TAG + ": " + getStackTraceString(e));
-                        }
-                    }
-                }
-        );
-
-        findAndHookMethod(
-                PM_CLASS,
-                lpparam.classLoader,
-                "getInstalledApplications",
-                long.class, int.class, int.class, /* flags, userId, callingUid */
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        try {
-                            List<ApplicationInfo> infos = (List<ApplicationInfo>) param.getResult();
-                            if (infos != null) {
-                                for (ApplicationInfo info : infos) {
-                                    checkAndMakeDebuggable(info, info.packageName, (int) param.args[1]);
-                                }
-                            }
-                        } catch (Exception e) {
-                            XposedBridge.log(LOG_TAG + ": " + getStackTraceString(e));
-                        }
-                    }
-                }
-        );
-
-        hookAllMethods(
-                Process.class,
-                "start",
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        final String niceName = (String) param.args[1];
-                        final int uid = (int) param.args[2];
-                        final int runtimeFlags = (int) param.args[5];
-
-                        final int user = UserHandle.getUserHandleForUid(uid).hashCode();
-                        if (isDebuggable(niceName, user))
-                            param.args[5] = runtimeFlags | DEBUG_ENABLE_JDWP;
-                    }
-                }
-        );
-    }
-
-    private void _hookPreTiramisu(final XC_LoadPackage.LoadPackageParam lpparam) {
-        final String PM_CLASS = "com.android.server.pm.PackageManagerService";
-
-        findAndHookMethod(
-                PM_CLASS,
-                lpparam.classLoader,
-                "getPackageInfo",
-                String.class, int.class, int.class, /* packageName, flags, userId */
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        try {
-                            PackageInfo packageInfo = (PackageInfo) param.getResult();
-                            if (packageInfo != null && packageInfo.applicationInfo != null)
-                                checkAndMakeDebuggable(packageInfo.applicationInfo, packageInfo.packageName, (int) param.args[2]);
-                        } catch (Exception e) {
-                            XposedBridge.log(LOG_TAG + ": " + getStackTraceString(e));
-                        }
-                    }
-                }
-        );
-
-        findAndHookMethod(
-                PM_CLASS,
-                lpparam.classLoader,
-                "getApplicationInfo",
-                String.class, int.class, int.class, /* packageName, flags, userId */
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        try {
-                            ApplicationInfo appInfo = (ApplicationInfo) param.getResult();
-                            if (appInfo != null)
-                                checkAndMakeDebuggable(appInfo, ((ApplicationInfo) param.getResult()).packageName, (int) param.args[2]);
-                        } catch (Exception e) {
-                            XposedBridge.log(LOG_TAG + ": " + getStackTraceString(e));
-                        }
-                    }
-                }
-        );
-
-        findAndHookMethod(
-                PM_CLASS,
-                lpparam.classLoader,
-                "getInstalledApplicationsListInternal",
-                int.class, int.class, int.class, /* flags, userId, callingUid */
-                new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        try {
-                            List<ApplicationInfo> infos = (List<ApplicationInfo>) param.getResult();
-                            if (infos != null) {
-                                for (ApplicationInfo info : infos) {
-                                    checkAndMakeDebuggable(info, info.packageName, (int) param.args[1]);
-                                }
-                            }
-                        } catch (Exception e) {
-                            XposedBridge.log(LOG_TAG + ": " + getStackTraceString(e));
-                        }
-                    }
-                }
-        );
-
-        hookAllMethods(
-                Process.class,
-                "start",
-                new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        final String niceName = (String) param.args[1];
-                        final int uid = (int) param.args[2];
-                        final int runtimeFlags = (int) param.args[5];
-
-                        final int user = UserHandle.getUserHandleForUid(uid).hashCode();
-                        if (isDebuggable(niceName, user))
-                            param.args[5] = runtimeFlags | DEBUG_ENABLE_JDWP;
-                    }
-                }
-        );
-    }
-
-    private void checkAndMakeDebuggable(ApplicationInfo appInfo, String packageName, int user) {
-        if (isDebuggable(packageName, user)) {
-            appInfo.flags |= FLAG_DEBUGGABLE;
-        }
-    }
 
     @SuppressLint("SdCardPath")
     private static boolean isDebuggable(final String packageName, int user) {
@@ -225,6 +46,131 @@ public class HookMain implements IXposedHookLoadPackage {
         boolean state = SELinuxHelper.getAppDataFileService().checkFileExists(path);
         StrictMode.setThreadPolicy(oldPolicy);
         return state;
+    }
+
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!"android".equals(lpparam.packageName))
+            return;
+        hook(lpparam);
+    }
+
+    private void hook(final XC_LoadPackage.LoadPackageParam lpparam) {
+        Class cls;
+        try {
+            cls = XposedHelpers.findClass("com.android.server.pm.ComputerEngine", lpparam.classLoader);
+        } catch (XposedHelpers.ClassNotFoundError error) {
+            XposedBridge.log(LOG_TAG + ": " + getStackTraceString(error));
+            return;
+        }
+        Method getPackageInfoMethod = findBestMethod(cls, "getPackageInfo");
+        if (getPackageInfoMethod == null) {
+            return;
+        }
+        XposedBridge.hookMethod(getPackageInfoMethod, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) {
+                try {
+                    PackageInfo packageInfo = (PackageInfo) param.getResult();
+                    if (packageInfo != null && packageInfo.applicationInfo != null)
+                        checkAndMakeDebuggable(packageInfo.applicationInfo, packageInfo.packageName, (int) param.args[2]);
+                } catch (Exception e) {
+                    XposedBridge.log(LOG_TAG + ": " + getStackTraceString(e));
+                }
+            }
+        });
+        XposedBridge.log(LOG_TAG + ": Hooked " + getPackageInfoMethod.toString());
+
+        Method getApplicationInfoMethod = findBestMethod(cls, "getApplicationInfo");
+        XposedBridge.hookMethod(getApplicationInfoMethod, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) {
+                try {
+                    ApplicationInfo appInfo = (ApplicationInfo) param.getResult();
+                    if (appInfo != null)
+                        checkAndMakeDebuggable(appInfo, ((ApplicationInfo) param.getResult()).packageName, (int) param.args[2]);
+                } catch (Exception e) {
+                    XposedBridge.log(LOG_TAG + ": " + getStackTraceString(e));
+                }
+            }
+        });
+        XposedBridge.log(LOG_TAG + ": Hooked " + getApplicationInfoMethod.toString());
+
+        Method getInstalledApplicationsMethod = findBestMethod(cls, "getInstalledApplications");
+        XposedBridge.hookMethod(getInstalledApplicationsMethod, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) {
+                try {
+                    List<ApplicationInfo> infos = (List<ApplicationInfo>) param.getResult();
+                    if (infos != null) {
+                        for (ApplicationInfo info : infos) {
+                            checkAndMakeDebuggable(info, info.packageName, (int) param.args[1]);
+                        }
+                    }
+                } catch (Exception e) {
+                    XposedBridge.log(LOG_TAG + ": " + getStackTraceString(e));
+                }
+            }
+        });
+        XposedBridge.log(LOG_TAG + ": Hooked " + getInstalledApplicationsMethod.toString());
+
+        Method startMethod = findBestMethod(Process.class, "start");
+        XposedBridge.hookMethod(startMethod, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) {
+                final String niceName = (String) param.args[1];
+                final int uid = (int) param.args[2];
+                final int runtimeFlags = (int) param.args[5];
+
+                final int user = UserHandle.getUserHandleForUid(uid).hashCode();
+                if (isDebuggable(niceName, user))
+                    param.args[5] = runtimeFlags | DEBUG_ENABLE_JDWP;
+            }
+        });
+        XposedBridge.log(LOG_TAG + ": Hooked " + startMethod.toString());
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            Method getInstalledApplicationsListInternalMethod = findBestMethod(cls, "getInstalledApplicationsListInternal");
+            XposedBridge.hookMethod(getInstalledApplicationsListInternalMethod, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) {
+                    try {
+                        List<ApplicationInfo> infos = (List<ApplicationInfo>) param.getResult();
+                        if (infos != null) {
+                            for (ApplicationInfo info : infos) {
+                                checkAndMakeDebuggable(info, info.packageName, (int) param.args[1]);
+                            }
+                        }
+                    } catch (Exception e) {
+                        XposedBridge.log(LOG_TAG + ": " + getStackTraceString(e));
+                    }
+                }
+            });
+            XposedBridge.log(LOG_TAG + ": Hooked " + cls.getName() + "." + getInstalledApplicationsListInternalMethod.toString());
+        }
+
+        XposedBridge.log(LOG_TAG + ": All methods hooked");
+    }
+
+    private Method findBestMethod(Class cls, String methodName) {
+        List<Method> getApplicationInfoList = new ArrayList<>();
+        for (Method m : cls.getDeclaredMethods()) {
+            if (m.getName().equals(methodName)) {
+                getApplicationInfoList.add(m);
+            }
+        }
+        if (getApplicationInfoList.size() == 0) {
+            XposedBridge.log(LOG_TAG + ": " + getStackTraceString(new NoSuchMethodError(cls.getName() + "." + methodName)));
+            return null;
+        }
+        getApplicationInfoList.sort((o1, o2) -> Integer.compare(o2.getParameterTypes().length, o1.getParameterTypes().length));
+        return getApplicationInfoList.get(0);
+    }
+
+    private void checkAndMakeDebuggable(ApplicationInfo appInfo, String packageName, int user) {
+        if (isDebuggable(packageName, user)) {
+            appInfo.flags |= FLAG_DEBUGGABLE;
+        }
     }
 
 }
